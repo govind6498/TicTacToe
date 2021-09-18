2e07011c15efbd89d59e1dec8cc3d703753f6432
govind6498 <govindsingh6498@gmail.com>
2021-09-18 15:25:56 +0530

    Added:Method to display Board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -6,7 +6,7 @@ public class TicTacToe {
 	public static Scanner sc = new Scanner(System.in);
 
 	static void createBoard() {
-		for(int index=0;index<10;index++) {
+		for(int index=1;index<10;index++) {
 			board[index]=' ';
 		}
 	}
@@ -27,9 +27,15 @@ public class TicTacToe {
 			System.out.println("Computer's choice is: "+computer);
 		}
 	}
+	static void showBoard() {
+		System.out.println(" "+board[1]+" | "+board[2]+" | "+board[3]+" | ");
+		System.out.println(" "+board[4]+" | "+board[5]+" | "+board[6]+" | ");
+		System.out.println(" "+board[7]+" | "+board[8]+" | "+board[9]+" | ");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welocme to Tic Tac Toe Game ");
 		createBoard();
-		getPlayerChoice();
+//		getPlayerChoice();
+		showBoard();
 	}
 }
